d6340cb5142fb540ac30cac1387b5e32405ddb08
Juan Antonio Quintana <juananonio19344@hotmail.com>
2019-09-25 14:15:50 +0100

    green: tax_identification_fields_are_mutually_exclusive

--- src/main/kotlin/csvFilter/CsvFilter.kt
+++ src/main/kotlin/csvFilter/CsvFilter.kt
@@ -6,24 +6,30 @@ class CsvFilter {
     fun filter(lines: List<String>): List<String> {
         val result  = mutableListOf<String>()
         result.add(lines[0])
+        val header = result.get(0)
         val invoice = lines[1]
         val fields = invoice.split(',')
-        val header = result.get(0)
         val grossFieldIndex = 2
         val netFieldIndex = 3
         val ivaFieldIndex = 4
         val igicFieldIndex = 5
+        val cifFieldIndex = 7
+        val nifFieldIndex = 8
         val ivaField = fields[ivaFieldIndex]
         val igicField = fields[igicFieldIndex]
         val grossField = fields[grossFieldIndex]
         val netField = fields[netFieldIndex]
+        val cifField = fields[cifFieldIndex]
+        val nifField = fields[nifFieldIndex]
         val decimalRegex = "\\d+(\\. \\d+)?".toRegex()
         val taxFieldsAreMutuallyExclusive =
             (ivaField.matches(decimalRegex)|| igicField.matches(decimalRegex)) &&
             (ivaField.isNullOrEmpty() || igicField.isNullOrEmpty())
 
+        val taxIdentificationFieldsAreMutuallyExclusive = cifField.isNullOrEmpty() || nifField.isNullOrEmpty()
+
         var net = 0.toBigDecimal()
-        if(taxFieldsAreMutuallyExclusive && header != ""){
+        if(taxFieldsAreMutuallyExclusive && !header.isNullOrEmpty() && taxIdentificationFieldsAreMutuallyExclusive){
             if (!ivaField.isNullOrEmpty()) net = grossField.toBigDecimal() - ((grossField.toBigDecimal() * ivaField.toBigDecimal())/100.toBigDecimal())
             if (!igicField.isNullOrEmpty()) net = grossField.toBigDecimal() - ((grossField.toBigDecimal() * igicField.toBigDecimal())/100.toBigDecimal())
             if(net == netField.toBigDecimal()) result.add(lines[1])
--- src/test/kotlin/csvFilter/CsvFilterShould.kt
+++ src/test/kotlin/csvFilter/CsvFilterShould.kt
@@ -70,6 +70,14 @@ class CsvFilterShould {
         assertThat(result).isEqualTo(listOf(""))
     }
 
+    @Test
+    fun tax_identification_fields_are_mutually_exclusive(){
+        val invoiceLine = "1,02/05/2019,1000,810,19,,ACER Laptop,B76430134,50584422Q"
+
+        val result = CsvFilter().filter(listOf(headerLine, invoiceLine))
+
+        assertThat(result).isEqualTo(listOf(headerLine))
+    }
 }
 
 
