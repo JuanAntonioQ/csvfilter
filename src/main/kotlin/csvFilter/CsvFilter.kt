52d4a3acd4bbc9c782f630282f3d790741980767
Juan Antonio Quintana <juananonio19344@hotmail.com>
2019-09-25 13:24:41 +0100

    green: lines_with_net_fields_correct_are_not_filtered

--- src/main/kotlin/csvFilter/CsvFilter.kt
+++ src/main/kotlin/csvFilter/CsvFilter.kt
@@ -1,23 +1,33 @@
 package csvFilter
 
+import java.math.BigDecimal
+
 class CsvFilter {
     fun filter(lines: List<String>): List<String> {
         val result  = mutableListOf<String>()
         result.add(lines[0])
         val invoice = lines[1]
         val fields = invoice.split(',')
+        val grossFieldIndex = 2
+        val netFieldIndex = 3
         val ivaFieldIndex = 4
         val igicFieldIndex = 5
         val ivaField = fields[ivaFieldIndex]
         val igicField = fields[igicFieldIndex]
+        val grossField = fields[grossFieldIndex]
+        val netField = fields[netFieldIndex]
         val decimalRegex = "\\d+(\\. \\d+)?".toRegex()
         val taxFieldsAreMutuallyExclusive =
             (ivaField.matches(decimalRegex)|| igicField.matches(decimalRegex)) &&
             (ivaField.isNullOrEmpty() || igicField.isNullOrEmpty())
 
+        var net = 0.toBigDecimal()
         if(taxFieldsAreMutuallyExclusive){
-            result.add(lines[1])
+            if (!ivaField.isNullOrEmpty()) net = grossField.toBigDecimal() - ((grossField.toBigDecimal() * ivaField.toBigDecimal())/100.toBigDecimal())
+            if (!igicField.isNullOrEmpty()) net = grossField.toBigDecimal() - ((grossField.toBigDecimal() * igicField.toBigDecimal())/100.toBigDecimal())
+            if(net == netField.toBigDecimal()) result.add(lines[1])
         }
+
         return result.toList()
     }
 
