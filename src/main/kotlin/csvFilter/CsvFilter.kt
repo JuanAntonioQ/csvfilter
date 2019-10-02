eff596eaac766fc35a3dcf9c701b0aa3f3acf1ec
Juan Antonio Quintana <juananonio19344@hotmail.com>
2019-10-02 15:24:51 +0100

    green: lines_repeated_are_filtered

--- src/main/kotlin/csvFilter/CsvFilter.kt
+++ src/main/kotlin/csvFilter/CsvFilter.kt
@@ -5,40 +5,45 @@ class CsvFilter {
         val result  = mutableListOf<String>()
         result.add(lines[0])
         val header = result.get(0)
-        val invoice = lines[1]
-        val fields = invoice.split(',')
-        val grossFieldIndex = 2
-        val netFieldIndex = 3
-        val ivaFieldIndex = 4
-        val igicFieldIndex = 5
-        val cifFieldIndex = 7
-        val nifFieldIndex = 8
-        val ivaField = fields[ivaFieldIndex]
-        val igicField = fields[igicFieldIndex]
-        val grossField = fields[grossFieldIndex]
-        val netField = fields[netFieldIndex]
-        val cifField = fields[cifFieldIndex]
-        val nifField = fields[nifFieldIndex]
-        val decimalRegex = "\\d+(\\. \\d+)?".toRegex()
-        val taxFieldsAreMutuallyExclusive =
-            (ivaField.matches(decimalRegex)|| igicField.matches(decimalRegex)) &&
-            (ivaField.isNullOrEmpty() || igicField.isNullOrEmpty())
 
-        val taxIdentificationFieldsAreMutuallyExclusive =
+        var linesNotRepeated = lines.distinct()
+        var linesWithoutHeader = linesNotRepeated.subList(1, linesNotRepeated.size)
+
+        for (line in linesWithoutHeader) {
+            val invoice = line
+            val fields = invoice.split(',')
+            val grossFieldIndex = 2
+            val netFieldIndex = 3
+            val ivaFieldIndex = 4
+            val igicFieldIndex = 5
+            val cifFieldIndex = 7
+            val nifFieldIndex = 8
+            val ivaField = fields[ivaFieldIndex]
+            val igicField = fields[igicFieldIndex]
+            val grossField = fields[grossFieldIndex]
+            val netField = fields[netFieldIndex]
+            val cifField = fields[cifFieldIndex]
+            val nifField = fields[nifFieldIndex]
+            val decimalRegex = "\\d+(\\. \\d+)?".toRegex()
+            val taxFieldsAreMutuallyExclusive =
+                (ivaField.matches(decimalRegex) || igicField.matches(decimalRegex)) &&
+                        (ivaField.isNullOrEmpty() || igicField.isNullOrEmpty())
+
+            val taxIdentificationFieldsAreMutuallyExclusive =
                 (cifField.isNullOrEmpty() || nifField.isNullOrEmpty()) &&
-                (!(cifField.isNullOrEmpty() && nifField.isNullOrEmpty()))
+                        (!(cifField.isNullOrEmpty() && nifField.isNullOrEmpty()))
 
-        var net = 0.toBigDecimal()
-        if(taxFieldsAreMutuallyExclusive               &&
-          !header.isNullOrEmpty()                      &&
-           taxIdentificationFieldsAreMutuallyExclusive &&
-          (cifField.length == 9 || nifField.length == 9)){
-            if (!ivaField.isNullOrEmpty()) net = grossField.toBigDecimal() - ((grossField.toBigDecimal() * ivaField.toBigDecimal())/100.toBigDecimal())
-            if (!igicField.isNullOrEmpty()) net = grossField.toBigDecimal() - ((grossField.toBigDecimal() * igicField.toBigDecimal())/100.toBigDecimal())
-            if(net == netField.toBigDecimal()) result.add(lines[1])
+
+            var net = 0.toBigDecimal()
+            if (taxFieldsAreMutuallyExclusive &&
+                !header.isNullOrEmpty() &&
+                taxIdentificationFieldsAreMutuallyExclusive &&
+                (cifField.length == 9 || nifField.length == 9)) {
+                if (!ivaField.isNullOrEmpty()) net = grossField.toBigDecimal() - ((grossField.toBigDecimal() * ivaField.toBigDecimal()) / 100.toBigDecimal())
+                else net = grossField.toBigDecimal() - ((grossField.toBigDecimal() * igicField.toBigDecimal()) / 100.toBigDecimal())
+                if (net == netField.toBigDecimal()) result.add(line)
+            }
         }
-
         return result.toList()
     }
-
 }
--- src/test/kotlin/csvFilter/CsvFilterShould.kt
+++ src/test/kotlin/csvFilter/CsvFilterShould.kt
@@ -97,6 +97,15 @@ class CsvFilterShould {
         assertThat(result).isEqualTo(listOf(headerLine, invoiceLine))
     }
 
+    @Test
+    fun lines_repeated_are_filtered(){
+        val invoiceLine = "1,02/05/2019,1000,810,19,,ACER Laptop,B76430134,"
+
+        val result = CsvFilter().filter(listOf(headerLine, invoiceLine, invoiceLine))
+        
+        assertThat(result).isEqualTo(listOf(headerLine, invoiceLine))
+    }
+
 }
 
 
